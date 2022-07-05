41866c769e3e1203a711c66da44de004e6ec891e
Marcos Danilo <marcosdanilo94@gmail.com>
2022-07-05 14:05:31 -0300

    fix: Conserta o bug que quebrou os requisito 04 e 10

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,13 +1,13 @@
 const errorMiddleware = (err, _req, res, _next) => {
   switch (err.message) {
     case '"name" length must be at least 5 characters long':
-      res.status(422).json(err.message);
+      res.status(422).json({ message: err.message });
       break;
     case '"name" is required':
-      res.status(400).json(err.message);
+      res.status(400).json({ message: err.message });
       break;
     case 'Product not found':
-      res.status(404).json(err.message);
+      res.status(404).json({ message: err.message });
       break;
     default:
       res.status(err.status).json(err.message);
